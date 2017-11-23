ce6e4928d8aa9e3fc94031f02d0260c435413565
aegospm <33882191+aegospm@users.noreply.github.com>
2017-11-23 10:51:29 -0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 // JavaScript Document
-function mensagem(){
-	window.alert("O Aplicativo está em construção");
+function olhar(){
+	window.alert("Olharei no fundo dos seus olhos e depois vc todinha.");
+}
+function tocar(){
+	window.alert("Tocarei primeiro em saus mãos e depois em vc todinha.");
+}
+function cheirar(){
+	window.alert("Darei um cheiro bem gostoso em vc.");
+}
+function abracar(){
+	window.alert("Darei abraços bem fortes...");
+}
+function beijar(){
+	window.alert("Começarei por seus lábios, depois seu corpo.");
+}
+function chupar(){
+	window.alert("Com todo prazer farei esse carinho em vc para deixar vc bem molhadinha. ;)");
+}
+function fuder(){
+	window.alert("Aí... surprise... soh precisa relaxar e deixar eu fazer o que penso. ;)");
+}
+function nda(){
+	window.alert("Tendi! Vc quis dizer a opção 7 ;)");
 }
