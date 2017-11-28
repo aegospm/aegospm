b5b24e0b83579890267db36ac9057a799bd42ba1
aegospm <33882191+aegospm@users.noreply.github.com>
2017-11-28 14:20:49 -0200

    Update index.php

--- index.php
+++ index.php
@@ -12,10 +12,10 @@
 
 - cadastrar as notas (eu tenoh que selecionar a turma...)
 -->
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta charset=utf-8" />
 	<title>SisAca</title>
 	<!--chamndo meus css aqui dentro-->
 		 <link href="css/modelo.css" rel="stylesheet" type="text/css"/>
